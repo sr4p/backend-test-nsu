090e32de055087db82f76005276bc6c7aecbefca
Sirawit <sirawit@edvisory.co.th>
2023-06-01 19:36:05 +0700

    eslint config

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const port = process.env.CONFIG_SERVERPORT || 3000
+  const port = process.env.CONFIG_SERVERPORT || 3000;
   const app = await NestFactory.create(AppModule);
   await app.listen(port);
-  console.log('####################################################################');
+  console.log(
+    '####################################################################',
+  );
 }
 bootstrap();
